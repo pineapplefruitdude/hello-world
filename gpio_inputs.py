09bbf4c7b1e6136ad43c53bcf3a95715c7b9a804
pineapplefruitdude <pineapplefruitdude@users.noreply.github.com>
2018-03-09 16:27:06 +0100

    weiter

--- gpio_inputs.py
+++ gpio_inputs.py
@@ -1,2 +1,39 @@
 import RPi.GPIO as GPIO
 import time
+
+#Zaehlweise der Pins festlegen
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(16, GPIO.IN, pull_up_down = GPIO.PUD.DOWN)
+
+def measure_time:
+    i = 0
+    j = 0
+    start_time = 0
+    a = True
+    while a = True:
+        if GPIO.input(18) == GPIO.HIGH:
+            channel18 = 1
+            i = i+1
+        if GPIO.input(18) == GPIO.LOW:
+            channel18 = 0
+
+        if GPIO.input(16) == GPIO.HIGH:
+            channel16 = 1
+            j = j+1
+        if GPIO.input(16) == GPIO.HIGH:
+            channel16 = 0
+
+        if i > 10 and j <= 10 and start_time == 0:
+            start_time = time.clock()
+        if j > 10 and i <= 10 and start_time == 0:
+            start_time = time.clock()
+        if i > 10 and j > 10:
+            end_time = time.clock()
+            delay = end_time - start_time
+            print "delay [s]", delay
+            a = False
+
+while True:
+    measure_time()
